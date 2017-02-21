5ba90240765a1d5db28913bb7ebfc7436b27f7b9
Adam J. Penn <madannep@gmail.com>
2017-02-20 20:17:47 -0800

    adding readme

--- p1/mm.c
+++ p1/mm.c
@@ -11,19 +11,23 @@
 
 #define COCKS_PER_SEC CLOCKS_PER_SEC;
 
+void calculate_dot(int** matrixA, int** matrixB, int** matrixC, int size);
+void print_result(int** matrixC, int size, char flag, double runTime);
+
 int main()
 {
-
+  //seed random value based off of time
+  srand(time(0));
+ 
+  //declaring variables
   int size, i, j, k, dot;
 	char flag;
   clock_t start, end;
   double runTime;
  
-  //seed random value based off of time
-  srand(time(0));
-  
 	if(!scanf("%c", &flag)){}
   if(!scanf("%d", &size)){};
+  start = clock();
 	int** matrixA = (int**)malloc(size * sizeof(int*));
 	int** matrixB = (int**)malloc(size * sizeof(int*));
 	int** matrixC = (int**)malloc(size * sizeof(int*));
@@ -62,7 +66,30 @@ int main()
     }
   }
 
-  start = clock();
+  calculate_dot(matrixA, matrixB, matrixC, size);
+  print_result(matrixC, size, flag, runTime);
+
+  end = clock();
+  runTime = ((double) (end - start)) / COCKS_PER_SEC;
+ 
+  return 0;
+}
+
+/**
+ * Calculates the dot product
+ *
+ * Takes in three matrixs and calculates the dot product of the first two and stores
+ * the reuslt into the third one.
+ *
+ * @param  matrixA    One of the matrixs being multiplied
+ * @param  matrixB    One of the matrixs being multiplied
+ * @param  matrixC    The matrix that holds the calculated dot product
+ * @param  size       The size of the matrix, only need one size sinces its n x n
+ * @return void 			Nothing is returned
+ */
+void calculate_dot(int** matrixA, int** matrixB, int** matrixC, int size) {
+  
+  int i, j, k, dot;
   
   // Calculate Dot product
   for (i = 0; i < size; i++) {
@@ -74,12 +101,23 @@ int main()
       matrixC[i][j] = dot;
     }
   }
- 
-  end = clock();
-  runTime = ((double) (end - start)) / COCKS_PER_SEC;
- 
+}
+
+/**
+ * Prints results
+ *
+ * Takes in the calculated dot product and prints it to the screen nicely formated
+ *
+ * @param  matrixC    The matrix that holds the calculated dot product
+ * @param  size       The size of the matrix, only need one size sinces its n x n
+ * @param  runTime    The calculated runtime of the program
+ * @return void 			Nothing is returned
+ */
+void print_result(int** matrixC, int size, char flag, double runTime) {
 	// Print out C
-	if (flag == 'I') {
+  int i, j;
+	
+  if (flag == 'I') {
 	  for (i = 0; i < size; i++) {
       for (j = 0; j < size; j++) {
         printf("%d ", matrixC[i][j]);
@@ -87,9 +125,11 @@ int main()
 	  	printf("\n");
 	  }
   }
-
   printf("Time to complete dot product: %f\n", runTime);
-
-  return 0;
 }
+
+
+
+
+
 
