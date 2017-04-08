a2e54fbb67325b7390801e1fe9498272396c403c
Adam J. Penn <madannep@gmail.com>
2017-04-08 15:42:53 -0700

    adding files so mark can tell me im not crazy

--- p3/matrixmult.c
+++ p3/matrixmult.c
@@ -34,11 +34,9 @@ int main() {
   int  size,
        start,
        end,
-       *matrixA = (int*)calloc(size*size, sizeof(int)),
-       *matrixB = (int*)calloc(size*size, sizeof(int)),
-       *matrixC = (int*)calloc(size*size, sizeof(int)),
-       *localMatrixA = (int*)calloc(size*size, sizeof(int)),
-       *localMatrixC = (int*)calloc(size*size, sizeof(int));
+       *matrixA = malloc(size*size*sizeof(int)),
+       *matrixB = malloc(size*size*sizeof(int)),
+       *matrixC = malloc(size*size*sizeof(int));
 
   // Get inital input from the user
   get_input(form, flag, &size, comm_rank);
@@ -47,7 +45,7 @@ int main() {
   // Fill in the matrixs
   if (comm_rank == 0) {
     
-  printf("Form: %s, Flag: %s, Size: %d\n", form, flag, size);
+    printf("Form: %s, Flag: %s, Size: %d\n", form, flag, size);
     
     if (strcmp(flag, "R") == 0) {
       random_fill(matrixA, matrixB, size);
@@ -55,49 +53,58 @@ int main() {
       input_fill(matrixA, matrixB, size);
     }
 
-  printf("Printing matrixA\n");
-  print_matrix(matrixA, size, -1);
-  printf("Printing matrixB\n");
-  print_matrix(matrixB, size, -1);
+    printf("running on %d processors\n", comm_sz);
   }
-
+  int *sendcounts = malloc(comm_sz * sizeof(*sendcounts));
 
   // Create barrier and start timer
   MPI_Barrier(MPI_COMM_WORLD);
   if (comm_rank == 0) start = MPI_Wtime();
 
-  /*
-  // Send matrixs
-  int *sendcounts = malloc(sizeof(int)*comm_rank);
-  int *displs = malloc(sizeof(int)*comm_rank);
+  // Calculate how much of matrixA to send
+  int *displs = malloc(comm_sz * sizeof(*displs));
 
   // calculate send counts and displacements
-  int rem = (size*size)%comm_sz, sum = 0;
+  int extra = size%comm_sz, sum = 0;
   for (int i = 0; i < size; i++) {
-    sendcounts[i] = (size*size)/comm_sz;
-    if (rem > 0) {
+    sendcounts[i] = size*size/comm_sz;
+    if (extra > 0) {
       sendcounts[i]++;
-      rem--;
+      extra--;
     }
 
     displs[i] = sum;
     sum += sendcounts[i];
   } 
-  */
-  //MPI_Scatterv(matrixA, sendcounts, displs, MPI_INT, localMatrixA, size*size, MPI_INT, 0,
-  MPI_Scatter(matrixA, size*size/comm_sz, MPI_INT, localMatrixA, size*size/comm_sz, MPI_INT, 0, MPI_COMM_WORLD); 
+
+  // Send matrixA and matrixB to all processors
   MPI_Bcast(matrixB, size*size, MPI_INT, 0, MPI_COMM_WORLD);
+  //MPI_Scatterv(matrixA, sendcounts, displs, MPI_INT, localMatrixA, sendcounts[comm_rank], MPI_INT, 0, MPI_COMM_WORLD);
+  if (comm_rank == 0) {
+    MPI_Scatter(matrixA, size*size/comm_sz, MPI_INT, MPI_IN_PLACE, size*size/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
+  } else {
+    MPI_Scatter(matrixA, size*size/comm_sz, MPI_INT, &matrixA[displs[comm_rank]], size*size/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
+
+  }
+
 
   // Calculate dot product
-  calculate_ijk(localMatrixA, matrixB, localMatrixC, size, size/comm_sz);
+  calculate_ijk(matrixA, matrixB, matrixC, size,
+                sendcounts[comm_rank]/comm_sz);
 
   // Gather the results back to process 0
-  //MPI_Gatherv(localMatrixC, sendcounts[comm_rank], MPI_INT, matrixC, sendcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Gather(localMatrixC, size*size/comm_sz, MPI_INT, matrixC, size*size/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
-   
+  //MPI_Gatherv(localMatrixC, sendcounts[comm_sz], MPI_INT, matrixC, sendcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
+  if (comm_rank == 0) {
+    MPI_Gather(MPI_IN_PLACE, size*size/comm_sz, MPI_INT, matrixC, size*size/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
+  } else {
+    MPI_Gather(&matrixC[displs[comm_rank]], size*size/comm_sz, MPI_INT, matrixC, size*size/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
+
+  }
   // Print the result
-  if (comm_rank == 0) end = MPI_Wtime();
-  print_matrix(matrixC, size, end-start);
+  if (comm_rank == 0) {
+    end = MPI_Wtime();
+    print_matrix(matrixC, size, end-start);
+  }
 
   MPI_Finalize();
 
@@ -143,14 +150,12 @@ void random_fill(int* matrixA, int* matrixB, int size) {
 
 void input_fill(int* matrixA, int* matrixB, int size) {
   // Declare variables and seed random generator
-  int i, input;
+  int i;
  
   // Fill matrix
   for (i = 0; i < size*size; i++) {
-    scanf("%d", &input);
-    matrixA[i] = input;
-    scanf("%d", &input);
-    matrixB[i] = input;
+    scanf("%d", &matrixA[i]);
+    scanf("%d", &matrixB[i]);
   }
 }
 
@@ -161,7 +166,7 @@ void calculate_ijk(int* matrixA, int* matrixB, int* matrixC, int size, int sendS
   // Calculate dot product using ijk form
   for (i = 0; i < sendSize; i++) {
     for (j = 0; j < size; j++) {
-      //matrixC[i*size+j] = 0;
+      matrixC[i*size+j] = 0;
       for (k = 0; k < size; k++) {
         matrixC[i*size+j] += matrixA[i*size+k] * matrixB[k*size+j];
       }
